f7e277570f5930f09ddde30169b6b35ed711a399
Dale Kanikkeberg <kanikkeberg.dale@student.greenriver.edu>
2024-04-09 13:42:38 -0400

    Added home page view

--- index.php
+++ index.php
@@ -14,7 +14,9 @@ $f3 = Base::instance();
 
 // Define a default route
 $f3->route('GET /', function() {
-    echo '<h1>Hello Fat-Free!</h1>';
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/home.html');
 });
 
 // Run Fat-Free
